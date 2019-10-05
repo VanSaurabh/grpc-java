ba6dc04145cdb112530716e5b1848507de483330
saurabh <saurabh.kundu93@gmail.com>
2019-10-05 21:11:00 +0530

    Adding Greeting Client initial code

---
+++ src/main/java/com/github/vansaurabh/grpc/client/GreetingClient.java
@@ -0,0 +1,25 @@
+package com.github.vansaurabh.grpc.client;
+
+import com.proto.dummy.DummyServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class GreetingClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello This is gRPC client");
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).build();
+        //creating stub
+        /*
+          synchronous stub
+         */
+        //DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
+        /*
+         * Asynchronous stub
+         */
+        //DummyServiceGrpc.DummyServiceFutureStub asyncClient = DummyServiceGrpc.newFutureStub(channel);
+        System.out.println("Shutting down channel");
+        channel.shutdown();
+    }
+}
